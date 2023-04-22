5c2cca83b133dbb5e9578158ef2503eeffc2020b
Juho Lee <eungseoni@gmail.com>
2023-04-22 20:45:59 +0900

    feat: make pages

---
+++ src/pages/Nav.js
@@ -0,0 +1,27 @@
+import { Link, Route, BrowserRouter, Routes } from "react-router-dom";
+import { useState } from "react";
+import React from "react";
+
+import "./nav.css";
+import logo from "../media/favicon.jpg";
+
+function App() {
+  return (
+    <div>
+      <header className="title-header">
+        <Link to="/" className="tltle-text">
+          <img src={logo} className="title-logo" alt="logo" />
+        </Link>
+
+        <Link to="About" className="tltle-text">
+          <span className="title-text2">About</span>
+        </Link>
+        <Link to="Products" className="tltle-text">
+          <span className="title-text2">Products</span>
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Products.js
@@ -0,0 +1,17 @@
+import { Link, Route, BrowserRouter, Routes } from "react-router-dom";
+import { useState } from "react";
+import React from "react";
+
+import "./products.css";
+
+function Home() {
+  return (
+    <div className="title">
+      <div className="body">
+        <h1>Team AI of MANGA</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
