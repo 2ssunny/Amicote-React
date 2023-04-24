84fe0a31e7fd9837367b41bb10ec6108f7af53f1
Juho Lee <eungseoni@gmail.com>
2023-04-24 22:51:04 +0900

    style,feat: add products page second one

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -7,6 +7,8 @@ import "./products.css";
 
 import p1 from "../media/DSC00671.JPG";
 import p2 from "../media/DSC00737.JPG";
+import p3 from "../media/DSC00250.JPG";
+import p4 from "../media/DSC00284.JPG";
 
 function Home() {
   const animatedItem = {
@@ -22,12 +24,24 @@ function Home() {
 
       <div className="product_1">
         <div className="product_image">
-          <img src={p1} className="p1_1_1" />
+          <img src={p1} className="p1_1" />
           <div className="imageempty"></div>
-          <img src={p2} className="p1_1_2" />
+          <img src={p2} className="p1_2" />
         </div>
         <div className="product_text">
           <h2>Product 1</h2>
+          <p>product</p>
+        </div>
+      </div>
+
+      <div className="product_2">
+        <div className="product_image">
+          <img src={p3} className="p2_1" />
+          <div className="imageempty"></div>
+          <img src={p4} className="p2_2" />
+        </div>
+        <div className="product_text">
+          <h2>Product 2</h2>
           <p>product</p>
         </div>
       </div>
